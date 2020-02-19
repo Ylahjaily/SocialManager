780e818f39b8c20ec7d9b3dc2894080705b628b8
anarayanamoorthy <a.narayanamoorthy@bmi-system.com>
2020-02-19 20:32:24 +0100

    [Evolution][BackAdminGetAllUsers] Add get all users method to the Api

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,9 +7,17 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractFOSRestController
 {
+    private $userRepo;
+
+    public function __construct(UserRepository $userRepo)
+    {
+        $this->userRepo=$userRepo;
+    }
+
     /**
      * @Rest\Get("/api/users/{email}")
      */
@@ -23,6 +31,7 @@ class UserController extends AbstractFOSRestController
      */
     public function getApiUsers()
     {
-        return $this->view("testAuth");
+        $users = $this->userRepo->findAll(); 
+        return $this->view($users);
     }  
 }
