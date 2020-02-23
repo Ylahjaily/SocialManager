29cd7bc928836eee0cd3414ac78ec8264dc07af4
anarayanamoorthy <a.narayanamoorthy@bmi-system.com>
2020-02-23 16:03:44 +0100

    [Evolution][BackReviewPostReview]Add a postReview method

--- src/Controller/ReviewController.php
+++ src/Controller/ReviewController.php
@@ -10,6 +10,9 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Repository\ReviewRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UserRepository;
+use App\Repository\ProposalRepository;
 
 class ReviewController extends AbstractFOSRestController
 {
@@ -37,5 +40,34 @@ class ReviewController extends AbstractFOSRestController
         return $this->view($review);
     }
 
+    /**
+     * @Rest\Post("/api/reviews/")
+     */
+    public function postApiReview(Request $request, ProposalRepository $proposalRepository, UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $review=new Review();
+        $review->setIsApproved(false);
+
+        if(!is_null($request->get('proposal_id'))) {
+            $proposal = $proposalRepository->find($request->get('proposal_id')); 
+            
+            if(!is_null($proposal)) {
+                $review->setProposalId($proposal);
+            }
+        }
+
+        if(!is_null($request->get('user_id'))) {
+            $user = $userRepository->find($request->get('user_id'));
+            if(!is_null($user)) {
+                $review->setUserId($user);
+            }
+        }
+        
+        $em->persist($review);
+        $em->flush();
+
+        return $this->view($review);
+    
+    }
 
 }
